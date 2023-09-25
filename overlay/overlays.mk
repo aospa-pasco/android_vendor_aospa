1e66f38fef2b1da8412353af2115eb2d83bb2731
rdxzv <rdxzv.dev@gmail.com>
2023-09-24 22:24:11 -0300

    fonts: Import Mi Lan Pro font
    Change-Id: Ib62a42647d1dc3d70618065c11cf90420ddf1382
    Signed-off-by: Redznn <h908659@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     FontGoogleSansLatoOverlay \
     FontHarmonySansOverlay \
     FontInterOverlay \
+    FontMiLanProOverlay \
     FontLinotteSourceOverlay \
     FontManropeOverlay \
     FontOnePlusOverlay  \
